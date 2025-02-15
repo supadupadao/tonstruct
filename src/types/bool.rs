b71cb32183a6eb9c86bc04c2754d79884b714acd
Suprun <hello@syoma.website>
2025-02-15 19:05:46 +0300

    feat: use built-in boolean type instead of custom (#10)
    **Summary of changes**:
    
    Use built-in `bool` type instead of custom struct

--- src/types/bool.rs
+++ src/types/bool.rs
@@ -2,72 +2,43 @@ use crate::utils::CastErrorToAnyhow;
 use crate::{FromCell, ToCell};
 use tonlib_core::cell::{CellBuilder, CellParser};
 
-#[derive(Debug, PartialEq, Default)]
-pub struct Bool(bool);
-
-impl From<bool> for Bool {
-    fn from(value: bool) -> Self {
-        Bool(value)
-    }
-}
-
-impl From<Bool> for bool {
-    fn from(value: Bool) -> Self {
-        value.0
-    }
-}
-
-impl ToCell for Bool {
+impl ToCell for bool {
     fn store<'a>(&self, builder: &'a mut CellBuilder) -> anyhow::Result<&'a mut CellBuilder> {
-        builder.store_bit(self.0).map_err_to_anyhow()
+        builder.store_bit(*self).map_err_to_anyhow()
     }
 }
 
-impl FromCell for Bool {
+impl FromCell for bool {
     fn load(parser: &mut CellParser) -> anyhow::Result<Self> {
-        parser.load_bit().map(Bool).map_err_to_anyhow()
+        parser.load_bit().map_err_to_anyhow()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_from() {
-        assert_eq!(Bool::from(true), Bool(true));
-    }
-
-    #[test]
-    fn test_into() {
-        assert_eq!(<Bool as Into<bool>>::into(Bool(true)), true);
-    }
-
-    #[test]
-    fn test_default() {
-        assert_eq!(Bool::default(), Bool(false));
-    }
+    use crate::types::bool;
 
     #[test]
     fn test_from_cell() {
         let cell = CellBuilder::new().store_bit(true).unwrap().build().unwrap();
 
-        assert_eq!(Bool::from_cell(cell).unwrap(), Bool(true));
+        assert_eq!(bool::from_cell(cell).unwrap(), true);
     }
 
     #[test]
     fn test_to_cell() {
         assert_eq!(
-            Bool(true).to_cell().unwrap(),
+            true.to_cell().unwrap(),
             CellBuilder::new().store_bit(true).unwrap().build().unwrap(),
         );
     }
 
     #[test]
     fn test_from_to_cell() {
-        let first_iter = Bool::from(true);
+        let first_iter = true;
         let cell = first_iter.to_cell().unwrap();
-        let second_iter = Bool::from_cell(cell).unwrap();
+        let second_iter = bool::from_cell(cell).unwrap();
 
         assert_eq!(first_iter, second_iter);
     }
